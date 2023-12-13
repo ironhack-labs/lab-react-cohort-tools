ba3f1cd3a1c08d9c8b21b70596955cbdf2b85ca4
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2023-12-13 15:40:11 +0100

    fully working lab

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import placeholderImage from "../assets/profile-icon.png";
+import students from "../assets/students.json";
 
 function StudentCard({
   _id,
@@ -11,6 +13,7 @@ function StudentCard({
   className,
 }) {
   return (
+    <Link to={`/students/${_id}`}>
       <div
         className={`StudentCard flex justify-between items-center p-3 mb-2 bg-white shadow-sm rounded border border-gray-200 hover:bg-gray-50 ${className}`}
       >
@@ -35,6 +38,7 @@ function StudentCard({
         <span style={{ flexBasis: "20%" }}>{email}</span>
         <span style={{ flexBasis: "20%" }}>{phone}</span>
       </div>
+    </Link>
   );
 }
 
