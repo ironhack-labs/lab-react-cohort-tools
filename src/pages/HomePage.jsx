c2dfbd283176cda815aca6887caf2fdb33feab17
BooCodeQuest <nightopshere@skiff.com>
2024-01-31 16:42:00 +0100

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,23 @@
-import { useState } from "react";
+import { Link } from 'react-router-dom';
 import StudentCard from "../components/StudentCard";
-
 import studentsData from "../assets/students.json";
+import { useState } from "react";
 
 function HomePage() {
-  // eslint-disable-next-line no-unused-vars
   const [students, setStudents] = useState(studentsData);
 
   return (
     <div className="border-2 border-rose-500 m-2">
-        <h1>Home Page</h1>
-        <div className="flex justify-between items-center p-2 font-bold border-b w-full">
-          <span className="flex items-center justify-center" style={{ flexBasis: "20%" }}>Image</span>
-          <span style={{ flexBasis: "20%" }}>Name</span>
-          <span style={{ flexBasis: "20%" }}>Program</span>
-          <span style={{ flexBasis: "20%" }}>Email</span>
-          <span style={{ flexBasis: "20%" }}>Phone</span>
-        </div>
+      <h1>Home Page</h1>
+     
 
-      {students &&
-        students.map((student) => {
-          return (
-              <StudentCard key={student._id} {...student} />
-          );
-        })}
+      {students.map((student) => (
+        <Link key={student._id} to={`/students/${student._id}`} style={{ textDecoration: 'none' }}>
+          <div className="m-2 hover:bg-gray-200">
+            <StudentCard {...student} />
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
