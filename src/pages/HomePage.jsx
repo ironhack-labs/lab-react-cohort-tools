b807c962c805b7656187b2b9a4dcb073901aa2c4
Haripriya <haripriyagct@gmail.com>
2024-02-07 16:23:09 +0100

    updated Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,8 +21,8 @@ function HomePage() {
       {students &&
         students.map((student) => {
           return (
-            <Link to={`/students/${student._id}`}>
-              <StudentCard key={student._id} {...student} />
+            <Link to={`/students/${student._id}`} key={student._id} >
+              <StudentCard {...student}  />
               </Link>
           );
         })}
