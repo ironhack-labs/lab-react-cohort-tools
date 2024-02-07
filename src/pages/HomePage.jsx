ddfdb7f539e3c34119942a133c0c927d04484926
Kosta Kallias <costakallias03@gmail.com>
2024-02-07 23:50:57 +0100

    iteratin 3+ 4 complete now

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
-
 import studentsData from "../assets/students.json";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   // eslint-disable-next-line no-unused-vars
@@ -21,7 +21,9 @@ function HomePage() {
       {students &&
         students.map((student) => {
           return (
+            <Link to={`/students/${student._id}`}>
               <StudentCard key={student._id} {...student} />
+            </Link>
           );
         })}
     </div>
