8baba9e0bfb12338b622e988305aec238dc3a723
Kumar Daryanani <destral@msn.com>
2024-02-07 14:55:37 +0100

    Updated lab based on feedback

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -7,8 +7,8 @@ function StudentDetailsPage() {
   // Find the current student profile by id.
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
-  const {studentId} = useParams();
-  const studentProfile = studentsData.find((student) => student._id === studentId);
+  const studentId = useParams().studentId;
+  const studentProfile = studentsData.find((student) => String(student._id) === studentId);
 
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-fuchsia-500 m-2">
