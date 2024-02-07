6605fc63ee053c9666d495b4b9df6fedfbdf6a1c
Laia Navalon <laianavalon@gmail.com>
2024-02-07 16:02:34 +0100

    Fix bug - Now Solved

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -9,7 +9,7 @@ function StudentDetailsPage() {
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
   const {studentId} = useParams()
-  const studentProfile = studentsData.find((student) => student._id === studentId);
+  const studentProfile = studentsData.find((student) => student._id == studentId);
 
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-fuchsia-500 m-2">
