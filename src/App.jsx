794faee05265212fbc9305822e76f03a454941ef
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-08 16:15:44 +0000

    lab solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import {Routes, Route} from 'react-router-dom';
+import studentsData from "./assets/students.json";
 
 
 function App() {
@@ -12,11 +14,13 @@ function App() {
       <Navbar />
 
       <div className="pages">
-        <HomePage />
+        <Routes>
+        <Route path="/" element={<HomePage />} />
 
-        <StudentDetailsPage />
+        <Route path="/students/:studentId" element={<StudentDetailsPage />} />
 
-        <UserProfilePage />
+        <Route path="/profile" element={<UserProfilePage />} />
+        </Routes>
       </div>
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "./../assets/logo-ironhack-blue.png";
+import { Link, NavLink } from "react-router-dom"; 
 
 function Navbar() {
   return (
@@ -19,15 +20,16 @@ function Navbar() {
         </div>
 
         <div className="w-1/4 flex justify-end mr-4">
+          <NavLink to ="/profile">
           {/* User Profile Button */}
-          <button className="flex items-center text-l py-1">
+          <button className="flex items-center text-l py-1" >
             <img
               src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
               alt="Logo"
               className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
             />
           </button>
-
+          </NavLink>
         </div>
       </div>
     </nav>
