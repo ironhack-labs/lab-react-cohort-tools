8ec57f7a19aebfacc8673f8a7d12372918103a26
Josep Climent <josepcliment92@gmail.com>
2024-02-15 21:03:34 +0100

    ultimos cambios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -22,7 +22,7 @@ function HomePage() {
       {students &&
         students.map((student) => {
           return (
-              <Link to={"/students/:studentId"}> <StudentCard key={student._id} {...student} /> </Link>
+              <Link to={`/students/${student._id}`}> <StudentCard key={student._id} {...student} /> </Link>
           );
         })}
     </div>
