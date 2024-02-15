5f2a33b1868edee7594740647c8f6b11134ab739
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-02-15 15:45:23 +0100

    solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
-
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
+      <div className="pages"></div>
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
 
-      <div className="pages">
-        <HomePage />
+        <Route path={"/:studentId"} element={<StudentDetailsPage />}/>
 
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
+        <Route path={"/profile"} element={<UserProfilePage />} />
+      </Routes>
     </div>
   );
 }
