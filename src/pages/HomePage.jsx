b0c63cd77fb2a17d01687d72b49c1831af205e00
Alexandra Busson <alexandra_b@live.fr>
2024-03-06 15:36:26 +0100

    iteration 4 missing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
 
-import studentsData from "../assets/students.json";
+// import studentsData from "../assets/students.json";
+import { Link} from "react-router-dom";
 
-function HomePage() {
+function HomePage({students}) {
   // eslint-disable-next-line no-unused-vars
-  const [students, setStudents] = useState(studentsData);
-
+  // const [students, setStudents] = useState(studentsData);
   return (
     <div className="border-2 border-rose-500 m-2">
         <h1>Home Page</h1>
+        
         <div className="flex justify-between items-center p-2 font-bold border-b w-full">
           <span className="flex items-center justify-center" style={{ flexBasis: "20%" }}>Image</span>
           <span style={{ flexBasis: "20%" }}>Name</span>
@@ -17,11 +18,14 @@ function HomePage() {
           <span style={{ flexBasis: "20%" }}>Email</span>
           <span style={{ flexBasis: "20%" }}>Phone</span>
         </div>
+        
 
       {students &&
         students.map((student) => {
           return (
-              <StudentCard key={student._id} {...student} />
+            <Link to={`/students/${student._id}`} key={student._id}>
+              <StudentCard key={student._id} {...student}  student={students}/>
+            </Link>
           );
         })}
     </div>
