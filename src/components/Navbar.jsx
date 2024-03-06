0bbb35ffcd304b0541d4b6f0135d6219b3fddd0b
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-03-06 15:26:11 +0100

    solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import logo from "./../assets/logo-ironhack-blue.png";
 
+import UserProfilePage from "../pages/UserProfilePage";
+import { Link } from 'react-router-dom'
+
 function Navbar() {
   return (
     <nav className="bg-blue-600 text-white shadow-md fixed top-0 left-0 w-full z-50">
@@ -7,9 +10,9 @@ function Navbar() {
         <div className="flex items-center space-x-2 w-1/4">
 
           {/* Home Button (Logo) */}
-          <button className="flex items-center text-l py-1">
+          <Link to="/" > <button className="flex items-center text-l py-1">
             <img src={logo} alt="Logo" className="h-8 w-auto" />
-          </button>
+          </button> </Link>
         </div>
 
         <div className="flex justify-center w-1/2">
@@ -20,13 +23,13 @@ function Navbar() {
 
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
-          <button className="flex items-center text-l py-1">
+          <Link to="/profile"> <button className="flex items-center text-l py-1">
             <img
               src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
               alt="Logo"
               className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
             />
-          </button>
+          </button> </Link>
 
         </div>
       </div>
