8e9336f818ca54e9c5258424e4da2212ffcec1ec
Piero Schenato <piero.schenato@gmail.com>
2024-03-06 15:32:34 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
+import React from 'react';
 import "./App.css";
-import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App relative z-20 pt-20">
-      <Navbar />
+      <nav>
+        <div className="logo">
+        <Link to="/"> Home Page </Link>
+        </div>
+        <div className="user-icon">
+      <Link to="/profile"> User Profile Page</Link>
+      </div>
+      </nav>
 
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/students/:studentId" element={<StudentDetailsPage/>} />;
+        <Route path="/profile" element={<UserProfilePage/>} />;
+      </Routes>
       <div className="pages">
         <HomePage />
 
         <StudentDetailsPage />
 
-        <UserProfilePage />
+        <UserProfilePage>
+        <div className='back-button'>
+        <Link to="/"> Home Page </Link>
+        <UserProfilePage/>
+        
+
+
       </div>
 
     </div>
