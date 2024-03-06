dd964236bf783caebfdb27149c3c286711231c02
Nwaigba66 <glorianwaigba@gmail.com>
2024-03-07 00:23:26 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -8,18 +9,26 @@ import UserProfilePage from "./pages/UserProfilePage";
 function App() {
 
   return (
+    
+   
     <div className="App relative z-20 pt-20">
       <Navbar />
-
+      
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+      <Routes>
+        
+        <Route path="/" element= {<HomePage />}/>
+       
+       
+        <Route path="/students/:studentId" element= {<StudentDetailsPage />} />
+        
+       
+        <Route path="/profile" element= {<UserProfilePage />} />
+        </Routes>
       </div>
-
     </div>
+   
+    
   );
 }
 
