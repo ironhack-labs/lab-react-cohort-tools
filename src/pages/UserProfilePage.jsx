cd7f7ec8c94a7a350cbe1963d9b38aa460fe748c
John Doe <johndoe@example.com>
2024-03-13 20:37:29 +0100

    maybe

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -32,11 +32,11 @@ function UserProfilePage() {
           </>
         )}
 
-        {/* Back button */}        
+        {/* Back button */}
         <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
           Back
         </button>
-        
+
       </div>
     </div>
   );
